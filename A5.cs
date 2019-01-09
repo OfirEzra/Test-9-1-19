4abaceaa683cbef3be35124c886b31236dfab3c0
OfirEzra <45737748+OfirEzra@users.noreply.github.com>
2019-01-09 18:12:34 +0200

    Add files via upload

---
+++ A5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+	class A5
+	{
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine(Numbers(4, 10, 12, -8, 111));
+		}
+		private static int Numbers(params int[] arrayOfNumber)
+		{
+			int sum = 0;
+			for (int i = 0; i < arrayOfNumber.Length; i++)
+			{
+				sum = sum + arrayOfNumber[i];
+			}
+			return sum;
+		}
+	}
+}
