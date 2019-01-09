00e2589f46e5ae36dd34f3f862daa7b85c60146c
OfirEzra <45737748+OfirEzra@users.noreply.github.com>
2019-01-09 17:36:29 +0200

    Add files via upload

---
+++ A1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+	class A1
+	{
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter a number:");
+			int x = Convert.ToInt32(Console.ReadLine());
+			Power2(ref x);
+			Console.WriteLine($"power 2 of your number is {x}");
+		}
+		private static void Power2(ref int i)
+		{
+			i = i * 2;
+		}
+	}
+}
